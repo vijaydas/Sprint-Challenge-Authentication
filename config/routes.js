ef91d1237ac9d94f3791b3498d4a792f429060ab
Vijay Das <vijaytdas@gmail.com>
2019-06-14 11:10:29 -0500

    jokes route testes with authorization header token - OK

--- config/routes.js
+++ config/routes.js
@@ -6,6 +6,7 @@ const { authenticate, generateToken } = require('../auth/authenticate');
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
+  //server.get('/api/jokes', authenticate, getJokes);
   server.get('/api/jokes', authenticate, getJokes);
 };
 
